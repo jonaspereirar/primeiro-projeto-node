8443f875c9cc2a4d069dcf08d1cb4949a393adab
jonaspereirar <jonaspereirar@gmail.com>
2020-04-19 02:10:02 +0100

    refac. add.: Repositório do TypeORM - database

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,29 +1,28 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all();
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     const parsedDate = parseISO(date);
 
-    const createApointment = new CreateAppointmentService(
-      appointmentsRepository,
-    );
+    const createApointment = new CreateAppointmentService();
 
-    const appointment = createApointment.execute({
+    const appointment = await createApointment.execute({
       provider,
       date: parsedDate,
     });
